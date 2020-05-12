bb638041b09bff07867e33e0e9066c5e6cf90432
PC-ROWIN\rowin <rowin_ruizendaal@hotmail.com>
2020-05-12 13:08:48 +0200

    Fresh start

--- client/state/utils/index.js
+++
@@ -1,7 +0,0 @@
-export const isDown = (game, key) => game.input.keyboard.isDown(key)
-export const createText = (game, target) =>
-  game.add.text(target.x, target.y, '', {
-    fontSize: '12px',
-    fill: '#FFF',
-    align: 'center'
-  })
