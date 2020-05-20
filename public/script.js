855d2bce47d660afcbf9723ed3fb7a99fa8631a2
Vuurvos1 <Vuurvos1@users.noreply.github.com>
2020-05-20 11:13:14 +0200

    rebuilding room system from bare minimum

--- public/script.js
+++ public/script.js
@@ -1,42 +1,73 @@
 let socket;
 socket = io.connect('localhost:3000');
-console.log(socket);
+
+
+// function setup() {
+//     console.log('yeeps')
+//         // createCanvas(400, 400);
+//         // background(0);
+//         // Start a socket connection to the server
+//         // Some day we would run this server somewhere else
+//     socket = io.connect('https://multiplayer-game-testing.herokuapp.com/');
+//     // socket = io.connect('localhost:3000');
+//     // We make a named event called 'mouse' and write an
+//     // anonymous callback function
+//     socket.on('mouse',
+//         // When we receive data
+//         function(data) {
+//             let str = window.location.search.slice(1);
+
+//             console.log(str);
+//             console.log(data.roomId)
+
+//             if (str == data.url && str != '') {
+//                 // console.log("Got: " + data.x + " " + data.y);
+//                 // Draw a blue circle
+//                 fill(0, 0, 255);
+//                 noStroke();
+//                 ellipse(data.x, data.y, 20, 20);
+//             }
+//         }
+//     );
+// }
+
+// setup();
 
 
 
-socket.on('connect', () => {
+// socket.on('connect', () => {
 
-    socket.emit('join', (err) => {
-        if (err) {
-            alert('something went wong');
-            window.location.href = '/';
-        } else {
-            console.log('no error 🦐');
-        }
-    });
+//     socket.emit('join', (err) => {
+//         if (err) {
+//             alert('something went wong');
+//             window.location.href = '/';
+//         } else {
+//             console.log('no error 🦐');
+//         }
+//     });
 
-    socket.on('disconnect', () => {
-        console.log('Disconected 🦀');
-    });
+//     socket.on('disconnect', () => {
+//         console.log('Disconected 🦀');
+//     });
 
-    socket.on('newMessage', (message) => {
-        console.log(message);
-    });
-});
+//     socket.on('newMessage', (message) => {
+//         console.log(message);
+//     });
+// });
 
 
 
-const sendButton = document.querySelector('#sendText');
-const message = document.querySelector('#textMessage');
+// const sendButton = document.querySelector('#sendText');
+// const message = document.querySelector('#textMessage');
 
-sendButton.addEventListener('click', (e) => {
-    e.preventDefault();
+// sendButton.addEventListener('click', (e) => {
+//     e.preventDefault();
 
-    let data = {
-        text: message.value
-    }
+//     let data = {
+//         text: message.value
+//     }
 
-    socket.emit('createMessage', data);
+//     socket.emit('createMessage', data);
 
-    message.value = '';
-});
+//     message.value = '';
+// });
