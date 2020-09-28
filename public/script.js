7fff1d58fc9caffaf700138a09c06c0ed15ca678
Sam de Man <47493725+Vuurvos1@users.noreply.github.com>
2020-09-28 12:18:46 +0200

    joining room image

--- public/script.js
+++ public/script.js
@@ -10,6 +10,8 @@ const roomSend = document.querySelector('#room button');
 const msgTxt = document.querySelector('#send input[type=text]');
 const msgSend = document.querySelector('#send button');
 
+const playerGrid = document.querySelector('.players .players__grid');
+
 // Incomming
 socket.on('joinRoom', (data) => {
   console.log(data);
@@ -21,6 +23,16 @@ socket.on('message', (data) => {
 
 socket.on('roomValue', (data) => {
   roomTxt.value = `${window.location.href}?${data}`;
+});
+
+socket.on('userJoin', (data) => {
+  playerGrid.innerHTML += `
+    <div class="players__user">
+      <img src="img/userImg/user${data}.png" alt="User photo ${data}" />
+      <h3>User ${data}</h3>
+      <h4></h4>
+    </div>
+`;
 });
 
 // Outgoing
@@ -40,4 +52,5 @@ msgSend.addEventListener('click', () => {
 document.addEventListener('DOMContentLoaded', () => {
   console.log('🚀');
   socket.emit('joinRoom', searchRoom);
+  // socket.emit('pickUser');
 });
--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const app = express();
 
 // Setup server
 let server = app.listen(process.env.PORT || 3000, () => {
+  // This call back just tells us that the server has started
+
   // let host = server.address().address;
   let port = server.address().port;
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-// This call back just tells us that the server has started
-function listen() {}
 
 app.use(express.static('public'));
 
@@ -20,12 +19,13 @@ let roomName = '';
 let io = require('socket.io')(server);
 
 io.on('connection', (socket) => {
-  console.log('We have a new client: ' + socket.id);
+  console.log(`We have a new client: ${socket.id}`);
 
   socket.on('joinRoom', (room) => {
     if (room) {
       roomName = room;
       socket.join(room);
+      socket.broadcast.emit('userJoin', ~~(Math.random() * 7));
     } else {
       console.log(socket.id);
       roomName = socket.id;
