81826ff783efa65dea2128b456a43be42ff54c24
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2021-11-15 20:11:12 +0100

    [feat] add canvas and drawing toolbox components

---
+++ frontend/src/components/molecules/DrawingToolbox.svelte
@@ -0,0 +1,80 @@
+<script>
+  const colors = [
+    '#FFFFFF',
+    '#C1C1C1',
+    '#EF130B',
+    '#FF7100',
+    '#FFE400',
+    '#00CC00',
+    '#00B2FF',
+    '#231FD3',
+    '#A300BA',
+    '#D37CAA',
+    '#A0522D',
+    '#000000',
+    '#4C4C4C',
+    '#740B07',
+    '#C23800',
+    '#E8A200',
+    '#005510',
+    '#00569E',
+    '#0E0865',
+    '#550069',
+    '#A75574',
+    '#63300D',
+  ];
+  let currentColor = colors[11];
+
+  const brushes = [3, 5, 10, 15, 20];
+  let currentBrush = brushes[0];
+</script>
+
+<div class="toolbox">
+  <!-- maybe further split toolbox parts -->
+  <div class="toolbox__colors flex flex-row">
+    <div
+      class="toolbox__colorPreview w-8 h-8 mr-4 rounded"
+      style={`background-color: ${currentColor}`}
+    />
+
+    <div class="toolbox__colorPicker grid grid-cols-11 w-max">
+      <!-- change this to radio buttons? -->
+      {#each colors as color}
+        <button
+          class="w-4 h-4"
+          style={`background-color: ${color}`}
+          on:click={() => {
+            currentColor = color;
+          }}
+        />
+      {/each}
+    </div>
+  </div>
+
+  <div class="toolbox__tools">
+    <!-- {#each items as item}
+    {/each} -->
+  </div>
+
+  <div class="toolbox__brushes flex flex-row">
+    {#each brushes as brush}
+      <button
+        class="w-8 h-8 grid place-items-center border border-black border-solid rounded"
+        class:active={brush == currentBrush}
+        on:click={() => {
+          currentBrush = brush;
+        }}
+        ><div
+          class="toolbox__brush rounded-full bg-black"
+          style={`width: ${brush}px;height: ${brush}px`}
+        /></button
+      >
+    {/each}
+  </div>
+</div>
+
+<style>
+  .active {
+    background-color: #e4e4e4;
+  }
+</style>
--- frontend/src/components/templates/game.svelte
+++ frontend/src/components/templates/game.svelte
@@ -1,5 +1,10 @@
 <script>
   // during game
+  import DrawingCanvas from '../molecules/DrawingCanvas.svelte';
+  import DrawingToolbox from '../molecules/DrawingToolbox.svelte';
 </script>
+
+<DrawingCanvas />
+<DrawingToolbox />
 
 <style></style>
