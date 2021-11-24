d5d19faf2972c06f666b2f733e1522acee064ea1
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2021-11-24 08:30:31 +0100

    [feat] add scss

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
 
 export default defineConfig({
   base: './',
   plugins: [
     svelte({
       /* plugin options */
+      preprocess: sveltePreprocess(),
     }),
   ],
 });
