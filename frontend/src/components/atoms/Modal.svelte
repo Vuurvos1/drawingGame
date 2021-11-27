ae71c53c653a3555a4efad32f6b8a43dfe46b8fd
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2021-11-27 09:57:08 +0100

    [feat] add more base components and remove files

---
+++ frontend/src/components/atoms/Modal.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let open = false;
+</script>
+
+{#if open}
+  <div>
+    <slot />
+  </div>
+{/if}
+
+<style></style>
---
+++ frontend/src/components/molecules/WordModal.svelte
@@ -0,0 +1,9 @@
+<script>
+  let words = [];
+</script>
+
+{#each words as word}
+  <button>{word}</button>
+{/each}
+
+<style></style>
